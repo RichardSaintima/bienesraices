5072cb2810d015abcfdd9330112200a2303b9333
Stanley Richard Saintima <105382783+RichardSaintima@users.noreply.github.com>
2023-12-16 23:00:14 -0300

    Update databases.php

--- includes/config/databases.php
+++ includes/config/databases.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectarBD() : mysqli{
-    $bd = new mysqli('localhost' , 'root' , 'Nachecael999' , 'bienesraices_crud');
+    $bd = new mysqli('localhost' , 'root' , 'admin' , 'bienesraices_crud');
 
     if(!$bd){
         echo "Error no se pudo conectar";
